0e6b1960c39c57946207aa0c6dbb74de0284e5ba
Steven Meisler <smeisler@g.harvard.edu>
2021-12-31 11:23:21 -0500

    Create submit_job_array.sh

---
+++ qsiprep/submit_job_array.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+subjs=$@
+base='' # SET THIS TO BE YOUR BIDS DIRECTORY
+
+# Get subject names from the directory
+if [[ $# -eq 0 ]]; then
+    pushd $base
+    subjs=($(ls sub-* -d))
+    popd
+fi
+
+# take the length of the array
+# this will be useful for indexing later
+len=$(expr ${#subjs[@]} - 1) # len - 1
+
+echo Spawning ${#subjs[@]} sub-jobs.
+
+sbatch --array=0-$len%100 $base/code/qsiprep/ss_qsiprep.sh $base ${subjs[@]}
