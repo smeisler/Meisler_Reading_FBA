d41e7c8f5636338bbdb1c690eed52fcfe4183294
Steven Meisler <smeisler@g.harvard.edu>
2022-06-28 13:12:18 -0400

    Update ss_qsiprep.sh
    only do recon

--- qsiprep/ss_qsiprep.sh
+++ qsiprep/ss_qsiprep.sh
@@ -3,11 +3,11 @@
 #SBATCH --mem=20GB
 #SBATCH --cpus-per-task=8
 #SBATCH -J qsiprep
-set -eu
+set -eu # Stop on errors
 
 ##### CHANGE THESE VARIABLES AS NEEDED ######
 IMG='' # put path to qsiprep docker image here
-module add openmind/singularity/3.6.3 # add singularity to path
+module add openmind/singularity/3.9.5 # add singularity to path
 scratch='' # assign working directory
 #############################################
 
@@ -30,9 +30,9 @@ ln -sf $bids_dir/dataset_description.json $scratch/${subject}_db/dataset_descrip
 ln -sf $bids_dir/$subject $scratch/${subject}_db/$subject
 
 # define the command
-cmd="singularity run -B ${scratch},${bids_dir},${output_dir} $IMG --participant_label ${subject:4} -w $scratch --fs-license-file ${bids_dir}/code/qsiprep/license.txt --unringing_method mrdegibbs --denoise_method patch2self --skip-bids-validation --output_resolution 1.25 $scratch/${subject}_db/ ${output_dir} participant"
+cmd="singularity run -B ${scratch},${bids_dir},${output_dir} $IMG --participant_label ${subject:4} -w $scratch --recon-only --recon-spec --fs-license-file ${bids_dir}/code/qsiprep/license.txt --skip-bids-validation $scratch/${subject}_db/ ${output_dir} participant"
 
 # run the command
-echo Submitted job for: ${subject}
-echo $'Command :\n'${cmd}
+echo "Submitted job for: ${subject}"
+echo "$'Command :\n'${cmd}"
 ${cmd}
