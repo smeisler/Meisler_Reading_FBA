2549327333cfbc4f8a00b1117f39aea9be65f6f5
Steven Meisler <smeisler@g.harvard.edu>
2022-06-28 23:00:30 -0400

    Update 1_individual_responses.sh

--- fba/bash_scripts/1_individual_responses.sh
+++ fba/bash_scripts/1_individual_responses.sh
@@ -7,7 +7,7 @@
 ########## Reorient subject data and fit their fiber response functions (FRFs) ##########
 
 # Get variables from config
-config=/om4/group/gablab/data/hbn_bids/code/fba/config
+config=/PATH/TO/config
 source $config
 
 #Stop on error
