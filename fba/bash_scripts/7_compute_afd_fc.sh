de483c6be1595f10ba542fd2a78b0463478173be
Steven Meisler <smeisler@g.harvard.edu>
2022-06-28 23:01:51 -0400

    Update 7_compute_afd_fc.sh

--- fba/bash_scripts/7_compute_afd_fc.sh
+++ fba/bash_scripts/7_compute_afd_fc.sh
@@ -7,7 +7,7 @@
 ########## Compute Fiber Density (FD), Cross-Section (FC) and their product (FDC) ##########
 
 # Get variables from config
-config=/om4/group/gablab/data/hbn_bids/code/fba/config
+config=/PATH/TO/config
 source $config
 
 # Stop on error
