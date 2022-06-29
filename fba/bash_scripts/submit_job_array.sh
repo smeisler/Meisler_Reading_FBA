1e41a96cf7bfd558ce5e9ad34b0fa5f7c01c40ba
Steven Meisler <smeisler@g.harvard.edu>
2022-06-28 23:02:58 -0400

    Update submit_job_array.sh

--- fba/bash_scripts/submit_job_array.sh
+++ fba/bash_scripts/submit_job_array.sh
@@ -3,7 +3,7 @@
 # RUN PROCESSING STEPS THAT RUN ON PARTICIPANTS INDIVIDUALLY IN PARALLEL
 
 # Get variables from config
-config=/om4/group/gablab/data/hbn_bids/code/fba/config
+config=/PATH/TO/config
 source $config
 
 subjs=$@
