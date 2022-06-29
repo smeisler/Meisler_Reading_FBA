fbff57657dc7155ec9779cb2de1291c73eb7d8b8
Steven Meisler <smeisler@g.harvard.edu>
2022-06-28 23:02:35 -0400

    Update 9_global_tract_stats.sh

--- fba/bash_scripts/9_global_tract_stats.sh
+++ fba/bash_scripts/9_global_tract_stats.sh
@@ -7,7 +7,7 @@
 ########## Calculate globally-averaged and tract-averaged fixel-stats ##########
 
 # Get variables from config
-config=/om4/group/gablab/data/hbn_bids/code/fba/config
+config=/PATH/TO/config
 source $config
 
 # Stop on error
